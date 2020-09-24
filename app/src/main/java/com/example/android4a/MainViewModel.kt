e0e1482a3ca46482a72a4c89f2cf6dacd5734494
sandro <sandroo97100@gmail.com>
2020-09-24 16:25:44 -0400

    tp2

---
+++ app/src/main/java/com/example/android4a/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.android4a
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+
+    val counter: MutableLiveData<Int> = MutableLiveData()
+    init {
+        counter.value =0
+    }
+
+    fun onClickedIncrement(){
+        counter.value =(counter.value ?:0)+1
+    }
+}
