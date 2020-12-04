208f863e0ddc4fdcd8787819229a5e8c24351d19
sandroooo <sandroo97100@gmail.com>
2020-12-04 20:29:25 +0100

    tp2

---
+++ app/src/main/java/com/example/android4a/presentation/main/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.android4a.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import com.example.android4a.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import org.koin.android.ext.android.inject
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val mainViewModel : MainViewModel by inject()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mainViewModel.loginLiveData.observe(this, Observer {
+            when(it){
+               is LoginSuccess ->{
+
+               }
+               LoginError ->{
+                   MaterialAlertDialogBuilder(this)
+                       .setTitle("Erreur")
+                       .setMessage("Compte inconnu")
+                       .setPositiveButton("OK"){dialog, wich->
+                           dialog.dismiss()
+                       }
+                       .show()
+               }
+            }
+        })
+        login_button.setOnClickListener{
+            mainViewModel.onClickedLogin(login_edit.text.toString().trim(),password_edit.text.toString())
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/android4a/presentation/main/MainViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.android4a.presentation.main
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.viewModelScope
+import com.example.android4a.domain.entity.User
+import com.example.android4a.domain.usecase.CreateUserUseCase
+import com.example.android4a.domain.usecase.GetUserUseCase
+import kotlinx.coroutines.*
+
+
+class MainViewModel (
+    private val createUserUseCase: CreateUserUseCase,
+    private val getUserUseCase: GetUserUseCase
+): ViewModel(){
+
+    val loginLiveData: MutableLiveData<LoginStatus> = MutableLiveData()
+
+    fun onClickedLogin(emailUser : String , password:String){
+        viewModelScope.launch(Dispatchers.IO) {
+            val user = getUserUseCase.invoke(emailUser)
+           val loginStatus =  if(user !=null){
+                LoginSuccess(user.email)
+            }else{
+                LoginError
+            }
+            withContext(Dispatchers.Main){
+                loginLiveData.value = loginStatus
+            }
+
+
+        }
+    }
+}
